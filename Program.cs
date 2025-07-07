aef8c17bb438fedb6553dbe244d4e14ef8866412
taixin-li <taixin-li@users.noreply.github.com>
2025-07-07 16:26:30 +0900

    ADD [Add Convertin Function]

--- Program.cs
+++ Program.cs
@@ -1,25 +1,80 @@
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.ClearProviders();
+builder.Logging.AddSimpleConsole(options =>
+{
+    options.SingleLine = true;
+    options.TimestampFormat = "yyyy/MM/dd HH:mm:ss ";
+});
+
 var app = builder.Build();
 
-app.MapPost("/", HandlePostRequest); 
+app.MapPost("/api/convert", (SensorInput input, ILogger<Program> logger) =>
+{
+    var response = ConvertSensorData(input);
+    logger.LogInformation($"受信: {input.SensorId}, 温度: {input.Temperature}");
+
+    return Results.Ok(response);
+
+    SensorResponse ConvertSensorData(SensorInput input)
+    {
+        var jstZone = TimeZoneInfo.FindSystemTimeZoneById("Asia/Tokyo");
+        var jstTime = TimeZoneInfo.ConvertTimeFromUtc(input.Timestamp, jstZone);
+
+        var response = new SensorResponse
+        {
+            Id = input.SensorId,
+            Data = new Data
+            {
+                Temp_c = input.Temperature,
+                Humidity_percent = input.Humidity
+            },
+            ReceivedAt = jstTime.ToString("yyyy-MM-ddTHH:mm:ssK")
+        };
+
+        if (input.Temperature > 100)
+        {
+            response.Warning = true;
+        }
+
+        return response;
+    }
+});
 
 app.Run();
 
-IResult HandlePostRequest(RequestBody body, string? apiKey, ILogger<Program> logger)
+// ----------- モデルクラス -----------
+
+class SensorInput
 {
-    if (apiKey != "hogehoge") return Results.Unauthorized();
-    logger.LogInformation("{question}", body.Question);
-    return Results.Ok(new Response { Answer = "(´・ω・｀)" });
+    [JsonPropertyName("sensorId")]
+    public string SensorId { get; set; }
+
+    [JsonPropertyName("temperature")]
+    public double Temperature { get; set; }
+
+    [JsonPropertyName("humidity")]
+    public int Humidity { get; set; }
+
+    [JsonPropertyName("timestamp")]
+    public DateTime Timestamp { get; set; }
 }
 
-class RequestBody
+class SensorResponse
 {
-    public string? Question { get; set; }
+    public string Id { get; set; }
+    public Data Data { get; set; }
+    public string ReceivedAt { get; set; }
+    public bool? Warning { get; set; }
 }
 
-class Response
+class Data
 {
-    public string? Answer { get; set; }
+    public double Temp_c { get; set; }
+    public int Humidity_percent { get; set; }
 }
